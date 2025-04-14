8f8b9f6554ed3ffb1c16f6498e929bd699b61269
Dean Papas <deanpapas@bigpond.com>
2025-04-14 22:32:19 +1000

    Implement Snap

--- lib/polyclip.dart
+++ lib/polyclip.dart
@@ -2,4 +2,8 @@ library polyclip;
 
 export 'vector.dart';
 export 'bbox.dart';
-
+export 'compare.dart';
+export 'snap.dart';
+export 'identity.dart';
+export 'orient.dart';
+export 'precision.dart';
